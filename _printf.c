f452adb0bfc749813a0bbe73dfd45e917e5f5f0f
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-17 15:42:58 -0700

    initail commit

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+  * _printf - produces output according to a format.
+  * @format: char pointer
+  *
+  * Return: an integer
+  */
+int _printf(const char *format, ...)
+{
+	
+}
